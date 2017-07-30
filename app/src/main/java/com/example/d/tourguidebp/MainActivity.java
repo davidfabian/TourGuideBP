006fc3bf649d473f3b856b8c0ce751bac6e894bd
davidfabian <fabiand.01@gmail.com>
2017-07-30 20:30:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/d/tourguidebp/MainActivity.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.d.tourguidebp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the points of interest (POI) category
+        TextView numbers = (TextView) findViewById(R.id.poi);
+        // Set a click listener for POI view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the POI View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, PoiActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        // Find the View that shows the restaurants category
+        TextView family = (TextView) findViewById(R.id.restaurants);
+        // Set a click listener for restaurants view
+        family.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the restaurants View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, RestaurantActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        // Find the View that shows the transport category
+        TextView colors = (TextView) findViewById(R.id.transport);
+        // Set a click listener for colors view
+        colors.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the transport View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this, TransportActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+
+        // Find the View that shows the phrases category
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        // Set a click listener for phrases view
+        phrases.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the phrases View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/d/tourguidebp/TransportActivity.java
@@ -0,0 +1,8 @@
+package com.example.d.tourguidebp;
+
+/**
+ * Created by d on 7/30/2017.
+ */
+
+public class TransportActivity {
+}
