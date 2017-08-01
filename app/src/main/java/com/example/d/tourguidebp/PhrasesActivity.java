94596cd4fbbb818209b98c6a182c81d2e0b475ca
davidfabian <fabiand.01@gmail.com>
2017-08-01 11:32:50 +0300

    hardcoded texts moved to strings.xml, java files refer to that.
    TODO:
    -remove hardcoded info from java code-get proper maps links
    -make icons for mainactivity

--- app/src/main/java/com/example/d/tourguidebp/PhrasesActivity.java
+++ app/src/main/java/com/example/d/tourguidebp/PhrasesActivity.java
@@ -20,23 +20,22 @@ public class PhrasesActivity extends AppCompatActivity {
         //populating the phrases list
         final ArrayList<TouristItem> destinations = new ArrayList<TouristItem>() {
             {
-                add(new TouristItem("Good morning!", "Jó reggelt!"));
-                add(new TouristItem("Good afternoon!", "Jó napot!"));
-                add(new TouristItem("Good evening!", "Jó estét!"));
-                add(new TouristItem("Good.", "Jó."));
-                add(new TouristItem("Bad.", "Rossz."));
-                add(new TouristItem("Yes.", "Igen."));
-                add(new TouristItem("No.", "Nem."));
-                add(new TouristItem("Thank you.", "Köszönöm."));
-                add(new TouristItem("Thanks'", "Köszi!"));
-                add(new TouristItem("Please.", "Kérem."));
-                add(new TouristItem("Police!", "Rendőr!"));
-                add(new TouristItem("Water!", "Víz"));
-                add(new TouristItem("Help!", "Segítség!"));
-                add(new TouristItem("Take me to....", "... akarok menni."));
-                add(new TouristItem("Very good!", "Nagyon jó!"));
-                add(new TouristItem("Not good.", "Nem jó."));
-
+                add(new TouristItem(getString(R.string.good_morning), getString(R.string.good_morning_hu)));
+                add(new TouristItem(getString(R.string.good_afternoon), getString(R.string.good_afternoon_hu)));
+                add(new TouristItem(getString(R.string.good_evening), getString(R.string.good_evening_hu)));
+                add(new TouristItem(getString(R.string.good), getString(R.string.good_hu)));
+                add(new TouristItem(getString(R.string.bad), getString(R.string.bad_hu)));
+                add(new TouristItem(getString(R.string.yes), getString(R.string.yes_hu)));
+                add(new TouristItem(getString(R.string.no), getString(R.string.no_hu)));
+                add(new TouristItem(getString(R.string.thank_you), getString(R.string.thank_you_hu)));
+                add(new TouristItem(getString(R.string.thanks), getString(R.string.thanks_hu)));
+                add(new TouristItem(getString(R.string.please), getString(R.string.please_hu)));
+                add(new TouristItem(getString(R.string.police), getString(R.string.police_hu)));
+                add(new TouristItem(getString(R.string.water), getString(R.string.water_hu)));
+                add(new TouristItem(getString(R.string.help), getString(R.string.help_hu)));
+                add(new TouristItem(getString(R.string.take_me_to), getString(R.string.take_me_to_hu)));
+                add(new TouristItem(getString(R.string.very_good), getString(R.string.very_good_hu)));
+                add(new TouristItem(getString(R.string.not_good), getString(R.string.not_good_hu)));
             }
         };
 
